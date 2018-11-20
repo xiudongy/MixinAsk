9bd708a73958f06dba465fbc1432705623104d3e
xiudongy <xiudongy@gmail.com>
2018-11-20 10:24:17 +0800

    add acess_token cookie and check the mobile

--- vendor/xiudongy/flarum-ext-auth-mixin/src/MixinAuthController.php
+++ vendor/xiudongy/flarum-ext-auth-mixin/src/MixinAuthController.php
@@ -18,6 +18,8 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Zend\Diactoros\Response\RedirectResponse;
 use Zamseam\Mixin\MixinClient;
 use Flarum\Core\User;
+use Illuminate\Support\Facades\Cache;
+
 
 class MixinAuthController implements ControllerInterface
 {
@@ -61,11 +63,19 @@ class MixinAuthController implements ControllerInterface
                 'session_id' => $session_id,
                 'private_key' => $private_key
             ]);
-            $mixin->setModel('oauth');
-            $result = $mixin->getOauthToken($client_id, $_GET['code'], $client_secret);
-            if(isset($result['data']['access_token'])) {
-                $token = $result['data']['access_token'];
-                //$_SESSION['mixin_access_token'] = $token;
+            if(isset($_COOKIE['mixin_access_token'])) {
+                $token = $_COOKIE['mixin_access_token']; 
+            } else {
+                $mixin->setModel('oauth');
+                $result = $mixin->getOauthToken($client_id, $_GET['code'], $client_secret);
+                if(isset($result['data']['access_token'])) {
+                    $token = $result['data']['access_token'];
+                    setcookie("mixin_access_token", $token, time()+3600);
+                } else {
+                    echo '<pre>';var_dump($result);exit;
+                }
+            }
+            if($token) {
                 $mixin->setModel('me');
                 $profile = $mixin->readProfile($token);
                 //$identification = ['email' => $profile['data']['identity_number'].'@miniwenda.com'];
@@ -75,7 +85,8 @@ class MixinAuthController implements ControllerInterface
                     'avatarUrl' => $profile['data']['avatar_url']
                 ];
                 $user = User::where('mixin_id', $profile['data']['identity_number'])->first();
-                if(!$user) {$user = new User;
+                if(!$user) {
+                    $user = new User;
                     $user->username = $profile['data']['full_name'];
                     $user->join_time = time();
                     $user->email = $profile['data']['identity_number'].'@vcdiandian.com'; 
@@ -84,12 +95,40 @@ class MixinAuthController implements ControllerInterface
                     $user->is_activated = 1;
                     $user->save();
                 }
-                $this->authResponse->make($request, $identification, $suggestions);
-                header("Location:/");
-                exit;
-            } else {
-                echo '<pre>';var_dump($result);exit;
+                $response = $this->authResponse->make($request, $identification, $suggestions);
+                if($this->isMobile()) {
+                    header("Location:/");
+                    exit;
+                }
+                return $response;
+            } 
+        }
+    }
+    protected function isMobile()
+    {
+        // 如果有HTTP_X_WAP_PROFILE则一定是移动设备
+        if (isset ($_SERVER['HTTP_X_WAP_PROFILE'])) {
+            return TRUE;
+        }
+        // 如果via信息含有wap则一定是移动设备,部分服务商会屏蔽该信息
+        if (isset ($_SERVER['HTTP_VIA'])) {
+            return stristr($_SERVER['HTTP_VIA'], "wap") ? TRUE : FALSE;// 找不到为flase,否则为TRUE
+        }
+        // 判断手机发送的客户端标志,兼容性有待提高
+        if (isset ($_SERVER['HTTP_USER_AGENT'])) {
+            $clientkeywords = [ 'mobile', 'nokia', 'sony', 'ericsson', 'mot', 'samsung', 'htc', 'sgh', 'lg', 'sharp', 'sie-', 'philips', 'panasonic', 'alcatel', 'lenovo', 'iphone', 'ipod', 'blackberry', 'meizu', 'android', 'netfront', 'symbian', 'ucweb', 'windowsce', 'palm', 'operamini', 'operamobi', 'openwave', 'nexusone', 'cldc', 'midp', 'wap' ];
+            // 从HTTP_USER_AGENT中查找手机浏览器的关键字
+            if (preg_match("/(" . implode('|', $clientkeywords) . ")/i", strtolower($_SERVER['HTTP_USER_AGENT']))) {
+                return TRUE;
             }
         }
+        if (isset ($_SERVER['HTTP_ACCEPT'])) { // 协议法，因为有可能不准确，放到最后判断
+            // 如果只支持wml并且不支持html那一定是移动设备
+            // 如果支持wml和html但是wml在html之前则是移动设备
+            if ((strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') !== FALSE) && (strpos($_SERVER['HTTP_ACCEPT'], 'text/html') === FALSE || (strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') < strpos($_SERVER['HTTP_ACCEPT'], 'text/html')))) {
+                return TRUE;
+            }
+        }
+        return FALSE;
     }
 }
